f020a11c06cf36f74ba8e5f74cc79423bc71bdbd
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-03-13 16:04:21 -0300

    feat: invalid email label

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ const btnGetStarted = document.getElementById("get-start-btn");
 const inputEmail = document.getElementById("email-input");
 const errorText = document.getElementById("form-error-msg");
 
+const errorIconHtml = '<i class="fa-regular fa-circle-xmark"></i> ';
+
 btnGetStarted.addEventListener('click', e => {
     e.preventDefault();
     
     const userEmail = inputEmail.value;
     if(userEmail.trim() == '') {
-        errorText.innerText = "Email is required";
+        inputEmail.classList.add('is-invalid');
+        errorText.innerHTML = `${errorIconHtml} Email is required`;
+
         return;
     }
 
